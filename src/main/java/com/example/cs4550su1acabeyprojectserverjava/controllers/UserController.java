924a6424c6bc583cec3f7d5a7345469ff5af0363
Andrew Cabey <me@acabey.xyz>
2020-06-21 12:23:19 -0400

    Added error return to User login

--- src/main/java/com/example/cs4550su1acabeyprojectserverjava/controllers/UserController.java
+++ src/main/java/com/example/cs4550su1acabeyprojectserverjava/controllers/UserController.java
@@ -2,17 +2,22 @@ package com.example.cs4550su1acabeyprojectserverjava.controllers;
 
 import com.example.cs4550su1acabeyprojectserverjava.models.User;
 import com.example.cs4550su1acabeyprojectserverjava.services.UserService;
+import org.apache.http.protocol.HTTP;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
 @CrossOrigin(
         origins = "http://localhost:3000",
+        allowedHeaders = "*",
         allowCredentials = "true")
 public class UserController {
     @Autowired
@@ -38,12 +43,23 @@ public class UserController {
     }
 
     @PostMapping("/api/login")
-    public User login(
+    public ResponseEntity login(
             @RequestBody User user,
             HttpSession session) {
+        User attemptedUser = service.findUserByUsername(user.getUsername());
+        if (attemptedUser == null) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body("User not found");
+        }
         User currentUser = service.findUserByCredentials(user.getUsername(), user.getPassword());
+        if (attemptedUser == null) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body("Invalid username or password");
+        }
         session.setAttribute("currentUser", currentUser);
-        return currentUser;
+        return ResponseEntity.status(HttpStatus.OK).body(currentUser);
     }
 
     @PostMapping("/api/logout")
